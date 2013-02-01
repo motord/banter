a1e4afe0cd1a391ca09d66c8272a574b6d38761d
Peter <motor@samdeha.com>
2013-02-01 23:56:13 +0800

    simhash

---
+++ application/weixin/fuzzy.py
@@ -0,0 +1,21 @@
+__author__ = 'peter'
+
+from application.hashes.simhash import simhash
+from google.appengine.api import memcache
+
+def decide(forktionary, message):
+    message_hash=simhash(message.split())
+    similarity=0
+    val=None
+    for key, value in forktionary.iteritems():
+        key_hash=memcache.get(key)
+        if not key_hash:
+            key_hash=simhash(key.split())
+            memcache.set(key, key_hash)
+            sim=message_hash.similarity(key_hash)
+            if sim>similarity:
+                similarity=sim
+                val=value
+    return val
+
+
